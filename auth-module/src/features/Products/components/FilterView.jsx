d2f37ebbf09a9f07b5bc5e399cc2c773a05d0e9e
omasu12 <46553890+omasu12@users.noreply.github.com>
2021-05-06 17:17:19 +0700

    up

--- auth-module/src/features/Products/components/FilterView.jsx
+++ auth-module/src/features/Products/components/FilterView.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 FilterView.propTypes = {
-    onChange:PropTypes.func,
-    filter:PropTypes.object,
+  onChange: PropTypes.func,
+  filter: PropTypes.object,
 };
 
-function FilterView({onChange = null, filter={}}) {
-    return (
-        <div>
-            
-        </div>
-    );
+
+const Filter_List = [
+  {
+    id: 1,
+    getLabel:'1',//element show name screen
+    isActive:1,//element check active
+    isVisible:1,// check element can't display
+    isRemovable:1,// check  element can't remove?
+    onRemove:1, // func handing remove
+    onToggle:1  // func handing toggle
+
+  },
+];
+
+function FilterView({ onChange = null, filter = {} }) {
+  return <div></div>;
 }
 
 export default FilterView;
